e9ab0602dddf99b200f3bd5be9a6568a9c6fc5bf
Julio Colon <julio.colon@microsoft.com>
2019-03-21 20:13:18 -0400

    Added comments about the variables.

--- azure/main.tf
+++ azure/main.tf
@@ -1,12 +1,13 @@
-module "azure-provider" {
+module "azure-provider" {  # load the Azure provider
   source = "./provider"
 }
 
-module "t1" {
+module "t1" { # run t1
     source = "./modules/t1"
 }
 
-module "t2" {
+module "t2" { # run t2
     source = "./modules/t2"
+    # Set the location of the t1 into the t2 variables
     location = "${module.t1.location}"
 }
